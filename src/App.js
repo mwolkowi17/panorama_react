9f64cd2917d3d818fb9e4b4339cc5690c5d4560e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-02-17 09:04:56 +0100

    action to strzalka added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, {useState} from 'react'
+import { Canvas, } from '@react-three/fiber'
 import { Panorama } from './Panorama_formatka';
 import { StrzalkaDiferent } from './StrzalkaDiferent';
 import { OrbitControls } from '@react-three/drei'
@@ -9,9 +9,10 @@ import { OrbitControls } from '@react-three/drei'
 
 
 
-function App() {
-const adress = ['./index.png']
 
+function App() {
+const adress = ['./index.png','./kawiarnia3.png']
+const [filenameAdres,SetAdress]=useState(adress[0])
   return (
     <>
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 5] }}>
@@ -20,8 +21,8 @@ const adress = ['./index.png']
         <pointLight position={[10, 10, 10]} />
         {/* <Box position={[20, -20, -50]} /> */}
         {/* <Strzalka position={[30,30,-50]} /> */}
-        <StrzalkaDiferent position={[55,-70,-140]} scale={[10,0.7,10]}/>
-        <Panorama position={[0, 0, 0]} filename={adress[0]} />
+        <StrzalkaDiferent position={[55,-70,-140]} scale={[10,0.7,10]}  onClick={(event) => SetAdress(adress[1])} />
+        <Panorama position={[0, 0, 0]} filename={filenameAdres}/>
         <OrbitControls />
 
       </Canvas>
